ea3f32a34dc50ff90c8a155f42e2f8533002fb10
jrshort89 <jrshort89@gmail.com>
2020-11-19 22:15:25 -0500

    done

--- main.js
+++ main.js
@@ -3,23 +3,43 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorMessage = document.getElementById('modal');
 
+let like = document.querySelectorAll('li > span');
+like.forEach(elm => {
+    elm.addEventListener('click', (event) => {
+        mimicServerCall()
+            .then(response => {
+                if (event.target.classList.contains('activated-heart') && event.target.textContent === FULL_HEART) {
+                    event.target.textContent = EMPTY_HEART;
+                } else {
+                    event.target.setAttribute('class', 'activated-heart');
+                    event.target.textContent = FULL_HEART;
+                }
+            }).catch(err => {
+                errorMessage.removeAttribute('class');
+                setTimeout(() => {
+                    errorMessage.setAttribute('class', 'hidden')
+                }, 5000);
 
+            })
+    });
+});
 
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            let isRandomFailure = Math.random() < .2
+            if (isRandomFailure) {
+                reject("Random server error. Try again.");
+            } else {
+                resolve("Pretend remote server notified of action!");
+            }
+        }, 300);
+    });
 }
